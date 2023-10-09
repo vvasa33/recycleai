f7a8425e827e18746cac6fb27a75139dc40702bd
NotXelay <59939509+NotXelay@users.noreply.github.com>
2023-10-08 23:51:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,54 @@ let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
-var model;
+let model;
+
+// Load the model asynchronously
+async function loadModel() {
+    model = await tf.loadLayersModel("model.json");
+}
+
 camera_button.addEventListener('click', async function() {
     let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
- video.srcObject = stream;
- model = await tf.loadLayersModel("model.json");
+
+    // Set the video stream as the source for the video element
+    video.srcObject = stream;
+
+    // Load the model after the camera is started
+    await loadModel();
 });
 
 click_button.addEventListener('click', function() {
+    // Draw the video frame on the canvas
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-    let image_data_url = canvas.toDataURL('image/jpeg');
 
-    const img = document.getElementById('canvas');
+    // Get the image data from the canvas
+    let image_data = canvas.toDataURL('image/jpeg');
 
-  result = predict(img);
-  console.log(result);
+    // Create an Image object and set its source to the canvas data URL
+    let img = new Image();
+    img.src = image_data;
+
+    // Call the predict function with the Image object
+    predict(img);
 });
-    
 
 async function predict(img) {
-  model = await loadLayersModel("model.json");
-  
-  const results = await model.predict(img);
-  model.dispose();
-  return results;
+    // Make sure the model is loaded before making predictions
+    if (!model) {
+        console.error("Model not loaded yet.");
+        return;
+    }
 
+    // Preprocess the image (resize, normalize, etc.) before making predictions
+    // You need to implement the preprocessing logic based on your model's requirements
+
+    // Assuming preprocessImage is a function that preprocesses the image for the model
+    // let preprocessedImage = preprocessImage(img);
+
+    // Make predictions
+    const results = await model.predict(preprocessedImage);
+
+    // Handle the prediction results here
+    console.log(results);
 }
